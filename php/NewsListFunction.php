de1723dd2f365c3b2aad05fc5f6425facbbd651f
ognjenstojanovic <ognjenstojanovic@hotmail.com>
2017-06-06 00:27:45 +0200

    Popravke naslovne strane

---
+++ php/NewsListFunction.php
@@ -0,0 +1,30 @@
+<?php
+
+function ListNews($category){
+    $servername = "localhost";
+    $username = "G23";
+    $password = "123";
+    $defaultDatabase = "portalvesti";
+        
+    // Create connection
+    $conn1 = new mysqli($servername, $username, $password, $defaultDatabase);
+    // Check connection
+    if ($conn1->connect_error) {
+        die("Connection failed: " . $conn->connect_error);		
+    }
+
+    $query = "SELECT * FROM Vesti WHERE Kategorija = '" . $category . "' OR Podkategorija = '" . $category . "'";
+
+    $result = $conn1->query($query);
+
+    echo '<ul>';
+
+    while($row = $result->fetch_assoc()){
+        echo '<li><a href=' . $row["Link"] . '>' . $row["Naslov"] . '</a></li>';
+    }
+
+    echo '</ul>';
+
+}
+
+?>
